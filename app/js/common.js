bca45f45aa5f47f3787b4e07e797f14f876f8a8c
PaulZSt <38794592+PaulZSt@users.noreply.github.com>
2019-05-21 16:47:48 +0300

    v. 0.3
    wdfsdftgdsfghdf

--- app/js/common.js
+++ app/js/common.js
@@ -8,3 +8,25 @@
         });
     });
 }(jQuery));
+
+$(function() {
+    var Accordion = function(el) {
+        $("ul li").has("ul").css({color: 'red'});
+        $("ul li").children("ul").hide();
+        $("ul li").click(function (e) {
+            $(this).children().slideToggle();
+        })
+        //console.log(el);
+        //this.el = el;
+
+        // more then one submenu open?
+        //this.multiple = multiple;
+
+        //var dropdownlink = this.el.find('.dropdownlink');
+        //dropdownlink.on('click',
+          //  { el: this.el, multiple: this.multiple },
+           // this.dropdown);
+    };
+
+    var accordion = new Accordion($('.accordion_menu'));
+})

62f0a6a1f36c981e06e1adcd36fab201a54bba12
PaulZSt <38794592+PaulZSt@users.noreply.github.com>
2019-06-11 17:49:55 +0300

    v.0.6
    fdgfgdg

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,40 @@
-(function ($) {
-    $(document).ready(function() {
-        $('.firstslide').owlCarousel({
-            items: 1,
-            nav: true,
-            dots: true,
-            loop: true
-        });
-    });
-}(jQuery));
-
 $(document).ready(function() {
-    function menu(selector){
+    (function () {
+        try {
+            $('.header__nav').on('click', function () {
+                $('.header__nav .header__nav_line').toggleClass('animate');
+            })
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    })();
+
+    (function ($) {
+        $(document).ready(function () {
+            $('.firstslide').owlCarousel({
+                items: 1,
+                nav: true,
+                dots: true,
+                loop: true
+            });
+        });
+    }(jQuery));
+
+
+    function menu(selector) {
         $(`${selector} ul`).css('display', 'none');
 
         var ul = $(`${selector} > li > a`).siblings('ul');
 
-        ul.siblings('a').on('click', function(e){
+        ul.siblings('a').on('click', function (e) {
             e.preventDefault();
             var subUl = $(this).siblings('ul');
             var activeLi = $(this).parent('li');
             activeLi.toggleClass('noborder');
             subUl.toggleClass('active');
 
-            if(subUl.hasClass('active')){
+            if (subUl.hasClass('active')) {
                 subUl.css('display', 'block');
             } else {
                 subUl.css('display', 'none');
